6fdf595971ebdf396872b3e85072a2792fd1bc1d
Solomon Watkins <silverwatkins@gmail.com>
2026-09-24 10:33:05 -0400

    fix: keep email escaping compatible with ES2020

--- lib/email.ts
+++ lib/email.ts
@@ -7,11 +7,11 @@ import { Resend } from 'resend';
 
 function escapeHtml(value: string): string {
   return value
-    .replaceAll('&', '&amp;')
-    .replaceAll('<', '&lt;')
-    .replaceAll('>', '&gt;')
-    .replaceAll('"', '&quot;')
-    .replaceAll("'", '&#039;');
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
 }
 
 function getResendClient() {
